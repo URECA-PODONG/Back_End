0b84b726ecf49e3e929b51e51a317092036fa8b3
parkyejin <uiop5809@naver.com>
2024-11-07 19:45:14 +0900

    CORS 에러 해겨 v4

--- src/main/java/com/ureca/sole_paradise/user/config/SecurityConfig.java
+++ src/main/java/com/ureca/sole_paradise/user/config/SecurityConfig.java
@@ -50,7 +50,7 @@ public class SecurityConfig {
 
                         CorsConfiguration config = new CorsConfiguration();
 
-                        config.setAllowedOrigins(List.of("*"));
+                        config.setAllowedOrigins(List.of("http://localhost:5173", "https://sole-paradise.vercel.app"));
                         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
                         config.setAllowCredentials(true);
                         config.setAllowedHeaders(List.of("*"));
