9a20d04ccf28c9ba176379dba719132f5f1abe97
YEJIN PARK <uiop5809@naver.com>
2024-11-07 19:46:33 +0900

    10차 버스 (#32)
    * test
    
    * 소셜로그인
    
    * PetItem:수정
    
    * PetItem:수정
    
    * PetItem:수정
    
    * gitignore logs
    
    * gitignore 수정
    
    * payment backend files upload
    
    * Delete logs directory
    
    * Update .gitignore
    
    * feat:쇼핑몰 기능 구현, 장바구니 수정 중
    
    * Delete logs directory
    
    * WalkRoute API
    
    * WalkRoute API
    
    * Develop branch 에러 해결
    
    * PetItem:수정
    
    * feat: 장바구니 기능 구현
    
    * feat:쇼핌몰 기능 구현 완성
    
    * WalkRoute BaseTimeEntity 상속 (#11)
    
    * conflict resolve
    
    * feat: pet api 경로 수정
    
    * feat: 실종등록 기능 완료
    
    * login/user/api
    
    * Health/PetItem:수정
    
    * Health/PetItem:수정
    
    * 소셜수정
    
    * Health/PetItem:수정
    
    * order 추가 및 삭제 수정 모두함
    
    * Health/PetItem:수정
    
    * Health/PetItem:수정
    
    * Health/PetItem:수정
    
    * Health/PetItem:수정
    
    * DB 실행 에러 해결
    
    * order/payment 수정
    
    * payment 수정
    
    * feat: userId에 따른 walkroute 조회 로직 구현
    
    * petItem:
    
    * petItem:
    
    * petItem:petIteController 유저/
    
    * feat: pet 이미지 등록 가능하게 세팅 완료
    
    * feat: pet 이미지 가능 부분 업로드
    
    * petItem:petIteController 유저/
    
    * fix: 프론트 배포 주소 CORS 해결
    
    * fix: 프론트 배포 주소 CORS 해결
    
    * Health @LastModifiedDate 삭제
    
    * CORS 에러 해결
    
    * CORS 에러 해겨 v2
    
    * CORS 에러 해겨 v3
    
    * CORS 에러 해겨 v4
    
    * CORS 에러 해겨 v4
    
    ---------
    
    Co-authored-by: ckdwns1221 <ckdwns2181@gmail.com>
    Co-authored-by: LeeSunghuck <hello.git.anaolfive@gmail.com>
    Co-authored-by: EunSeok <dldmstjr114@naver.com>
    Co-authored-by: Suh-code <yunkisuh@naver.com>
    Co-authored-by: ckdwns1221 <130385395+ckdwns1221@users.noreply.github.com>
    Co-authored-by: subinsong01 <haely97@naver.com>
    Co-authored-by: SUBIN <134045937+subinsong01@users.noreply.github.com>
    Co-authored-by: Seonghyuk Lee <qktkrdl5@naver.com>

--- src/main/java/com/ureca/sole_paradise/user/config/SecurityConfig.java
+++ src/main/java/com/ureca/sole_paradise/user/config/SecurityConfig.java
@@ -49,8 +49,7 @@ public class SecurityConfig {
                     public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
 
                         CorsConfiguration config = new CorsConfiguration();
-
-                        config.setAllowedOrigins(List.of("*"));
+                        config.setAllowedOrigins(List.of("http://localhost:5173", "https://sole-paradise.vercel.app"));
                         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
                         config.setAllowCredentials(true);
                         config.setAllowedHeaders(List.of("*"));
